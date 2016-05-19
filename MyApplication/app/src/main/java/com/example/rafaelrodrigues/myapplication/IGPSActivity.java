ca54e6c70a525bb73966638ea9bab2ec92029a5a
unknown <rafa9000@gmail.com>
2016-05-19 10:46:38 -0300

    removendo projeto

--- MyApplication/app/src/main/java/com/example/rafaelrodrigues/myapplication/IGPSActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.rafaelrodrigues.myapplication;
-
-/**
- * Created by rafael.rodrigues on 17/05/2016.
- */
-public interface IGPSActivity {
-    public void locationChanged(double longitude, double latitude);
-    public void displayGPSSettingsDialog();
-}
